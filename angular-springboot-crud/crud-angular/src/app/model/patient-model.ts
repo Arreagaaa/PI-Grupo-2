4b488147a57faa6905e4d597648ab86c8b9de125
Arreagaa <144147382+Arreagaaa@users.noreply.github.com>
2025-07-31 08:37:57 -0600

    fix commit

--- angular-springboot-crud/crud-angular/src/app/model/patient-model.ts
+++ angular-springboot-crud/crud-angular/src/app/model/patient-model.ts
@@ -1,3 +1,5 @@
+// modelo de datos para el paciente
+// angular-springboot-crud/crud-angular/src/app/model/patient-model.ts
 export class PatientModel {
   patientID: number = 0;
   name: string = '';
